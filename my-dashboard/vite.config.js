172e8bdd8079cf3acaa6b96b3cf683bd6a9e8371
Deo <januahlaritacoronelbaldeo@gmail.com>
2025-07-01 13:25:00 -0500

    fix

--- my-dashboard/vite.config.js
+++ my-dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.my-dashboard || "/my-dashboard"
+  base: process.env.my-dashboard || "/my_dashboard"
 })
